135e115c0a361b8a68c71a7a163b0dfa4aa72097
Brianna Fidder <bfidder@mines.edu>
2013-03-30 17:25:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabletopRPG::Application.config.secret_token = '3b24c4ad4c8406ad8fdc52f3fddbf961245e9007daf97d10b082476f675b1a5a51a25bd25cbc6904118e90fec1079fe58bd2a8d2c25182a2786d4e686c359455'
